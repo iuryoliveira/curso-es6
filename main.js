d1f1eee9b097d23f1c4cb8457dce189b76326516
Iury <iury@rarolabs.com.br>
2020-03-09 23:01:18 -0300

    Classes

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
-
-class Teste {
-    metodo() {
-
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    outro() {
-        
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.usuario = 'Iury';
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById("novotodo").onclick = function() {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
+
+class Matematica {
+    constructor() {
+        this.todos = [];
+    }
+
+    static soma(a, b) {
+        return a + b;
+    }
+}
+
+console.log(Matematica.soma(1, 2));
