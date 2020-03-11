ae4f349da253c37f69551adf90069ea9f762108a
Iury <iury@rarolabs.com.br>
2020-03-11 00:02:18 -0300

    exercicio 1

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const nome = 'Iury';
-const idade = '22';
+class Usuario {
+    constructor(email, senha) {
+        this.email = email;
+        this.senha = senha;
+    }
 
-const usuario = {
-    nome,
-    idade,
-    empresa: 'Raro'
-};
+    isAdmin() {
+        return this.admin ? true : false;
+    }
+}
 
-console.log(usuario);
+class Admin extends Usuario {
+    constructor() {
+        super();
+        this.admin = true;
+    }
+}
+
+const User1 = new Usuario('email@teste.com', 'senha123');
+const Adm1 = new Admin('email@teste.com', 'senha123');
+console.log(User1.isAdmin()) // false
+console.log(Adm1.isAdmin()) // true
