4c40a6687b4701cb15f01306189df04797e44007
Iury <iury@rarolabs.com.br>
2020-03-10 23:51:34 -0300

    Operadores rest/spread

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
+//REST
+
 const usuario = {
     nome: 'Iury',
     idade: 22,
-    endereco: {
-        cidade: 'Santa Luzia',
-        estado: 'MG'
-    },
-};
-
-const { nome, idade, endereco: { cidade } } = usuario;
-
-console.log(nome);
-console.log(idade);
-console.log(cidade);
-
-function mostraNome({ nome, idade }) {
-    console.log(nome);
-    console.log(idade);
+    empresa: 'Raro'
 }
 
-mostraNome(usuario);
+const { nome, ...resto } = usuario;
+
+console.log(nome);
+console.log(resto);
+
+const arr = [1, 2, 3, 4];
+
+const [ a, b, ...c ] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+function soma(a, b,...params) {
+    return a + b + params.reduce((total, next) => total + next);
+}
+
+console.log(soma(1,2,3,4));
+
+//SPREAD
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [ ...arr1, ...arr2 ];
+console.log(arr3);
+
+const pessoa1 = {
+    nome: 'Iury',
+    idade: 22,
+    empresa: 'Raro'
+}
+
+const pessoa2 = { ...pessoa1, nome: 'José' };
+console.log(pessoa2);
