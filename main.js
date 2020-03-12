998cac8b3adca1771f64136f77a92dda56889cb1
Iury <iury@rarolabs.com.br>
2020-03-11 23:20:01 -0300

    exercicio 6

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-//5.1 REST
-const arr = [1, 2, 3, 4, 5, 6];
-
-const [ x, ...y ] = arr;
-
-console.log(x); // 1
-console.log(y); // [2, 3, 4, 5, 6]
-
-function soma(...params) {
-    return params.reduce((total, next) => total + next);
-}
-
-console.log(soma(1, 2, 3, 4, 5, 6)); // 21
-console.log(soma(1, 2)); // 3
-
-//5.2 SPREAD
-const usuario = {
-    nome: 'Diego',
-    idade: 23,
-    endereco: {
-        cidade: 'Rio do Sul',
-        uf: 'SC',
-        pais: 'Brasil',
-    }
-};
-
-const usuario2 = { ...usuario, nome: 'Gabriel' };
-const usuario3 = { ...usuario, endereco: { cidade: 'Lontras' } };
-
-console.log(usuario2);
-console.log(usuario3);
+const usuario = 'Diego';
+const idade = 23;
+console.log(`O usuário ${usuario} possui ${idade} anos`);
