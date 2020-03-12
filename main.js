98ee27d604737a51844d37171bf540812e850ee5
Iury <iury@rarolabs.com.br>
2020-03-11 23:20:51 -0300

    exercicio 7

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const usuario = 'Diego';
+const nome = 'Diego';
 const idade = 23;
-console.log(`O usuário ${usuario} possui ${idade} anos`);
+
+const usuario = {
+    nome,
+    idade,
+    cidade: 'Rio do Sul'
+}
