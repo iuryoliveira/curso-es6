eb3b92423d5a5a6ed7ba1d4e3fddb4b7d51f3d56
Iury <iury@rarolabs.com.br>
2020-03-11 23:43:48 -0300

    import/export

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-import { soma } from './funcoes';
+import somaNumeros, { sub } from './soma';
+import * as funcoes from './funcoes';
 
-console.log(soma(1,2));
+console.log(somaNumeros(1,2));
+console.log(sub(4,2));
+
+console.log(funcoes.soma(5, 4));
+console.log(funcoes.sub(5, 4));
+console.log(funcoes.mult(5, 4));
