9f6129295d71a124aa5cbb452315f88cd7e27e76
Iury <iury@rarolabs.com.br>
2020-03-11 23:51:32 -0300

    webpack dev server

--- main.js
+++
@@ -1,9 +0,0 @@
-import somaNumeros, { sub } from './soma';
-import * as funcoes from './funcoes';
-
-console.log(somaNumeros(1,2));
-console.log(sub(4,2));
-
-console.log(funcoes.soma(5, 4));
-console.log(funcoes.sub(5, 4));
-console.log(funcoes.mult(5, 4));
