324d580b6aae3e6500bad6955e0a3b51e31b70c0
Iury <iury@rarolabs.com.br>
2020-03-11 23:19:08 -0300

    exercicio 5

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-//4.1
-const empresa = {
-    nome: 'Rocketseat',
-    endereco: {
-    cidade: 'Rio do Sul',
-    estado: 'SC',
-    }
-   };
+//5.1 REST
+const arr = [1, 2, 3, 4, 5, 6];
 
-const { nome, endereco: { cidade, estado} } = empresa;
+const [ x, ...y ] = arr;
 
-console.log(nome); // Rocketseat
-console.log(cidade); // Rio do Sul
-console.log(estado); // SC
+console.log(x); // 1
+console.log(y); // [2, 3, 4, 5, 6]
 
-//4.2
-function mostraInfo({ nome, idade }) {
-    return `${nome} tem ${idade} anos.`;
+function soma(...params) {
+    return params.reduce((total, next) => total + next);
 }
 
-console.log(mostraInfo({ nome: 'Diego', idade: 23 }));
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+console.log(soma(1, 2)); // 3
+
+//5.2 SPREAD
+const usuario = {
+    nome: 'Diego',
+    idade: 23,
+    endereco: {
+        cidade: 'Rio do Sul',
+        uf: 'SC',
+        pais: 'Brasil',
+    }
+};
+
+const usuario2 = { ...usuario, nome: 'Gabriel' };
+const usuario3 = { ...usuario, endereco: { cidade: 'Lontras' } };
+
+console.log(usuario2);
+console.log(usuario3);
