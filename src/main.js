7bd024a872c96dc58fc467fd89676c8ba7e3280b
Iury <iury@rarolabs.com.br>
2020-03-14 19:01:53 -0300

    Render em tela

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ class App {
     constructor() {
         this.repositories = [];
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
         this.registerHandlers();
     }
 
@@ -19,7 +20,33 @@ class App {
             html_url: '#'
         });
 
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+            
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name))
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+            
+            let itemEl = document.createElement('li');
+            itemEl.appendChild(imgEl);
+            itemEl.appendChild(titleEl);
+            itemEl.appendChild(descriptionEl);
+            itemEl.appendChild(linkEl);
+            
+            this.listEl.appendChild(itemEl);
+        });
     }
 }
 
