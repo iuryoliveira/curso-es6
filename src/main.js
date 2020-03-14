a3ef9dd31378cfc6a2b9f916b388f0179a35ba75
Iury <iury@rarolabs.com.br>
2020-03-14 00:10:48 -0300

    es2020 features

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 alert('Funcionando');
+
+let pessoa = {
+    nome: 'Iury',
+    idade: 22
+}
+
+pessoa.show = false;
+console.log(pessoa.show ?? true);
+
+let user = {};
+user.address = {street: 'londrina'};
+let street = user?.address?.street;
+console.log(street);
