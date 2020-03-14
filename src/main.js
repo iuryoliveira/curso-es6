796281816b48c291d0fce5b18c33a561c90040a2
Iury <iury@rarolabs.com.br>
2020-03-14 17:28:54 -0300

    Async/await

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-alert('Funcionando');
-
 let pessoa = {
     nome: 'Iury',
     idade: 22
@@ -12,3 +10,14 @@ let user = {};
 user.address = {street: 'londrina'};
 let street = user?.address?.street;
 console.log(street);
+
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK') }, 2000);
+});
+
+async function executaPromise() {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+}
+
+executaPromise();
