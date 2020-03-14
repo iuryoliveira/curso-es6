06fa38d4f0f26a38916bd00baa10d465bfbe5f14
Iury <iury@rarolabs.com.br>
2020-03-14 17:45:22 -0300

    Configurando axios

--- src/main.js
+++ src/main.js
@@ -1,23 +1,15 @@
-let pessoa = {
-    nome: 'Iury',
-    idade: 22
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(userName) {
+        try {
+            const response = await axios.get(`https://api.github.com/users/${userName}`);
+            
+            console.log(response);
+        } catch (err) {
+            console.warn('Erro na API');
+        }
+    }
 }
 
-pessoa.show = false;
-console.log(pessoa.show ?? true);
-
-let user = {};
-user.address = {street: 'londrina'};
-let street = user?.address?.street;
-console.log(street);
-
-const minhaPromise = () => new Promise((resolve, reject) => {
-    setTimeout(() => { resolve('OK') }, 2000);
-});
-
-async function executaPromise() {
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-}
-
-executaPromise();
+Api.getUserInfo('iuryoliveira');
