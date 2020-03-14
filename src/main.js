d372f084b4f8a698cd72a35b13c5cb9d8547a057
Iury <iury@rarolabs.com.br>
2020-03-14 17:55:45 -0300

    Exercícios async await

--- src/main.js
+++ src/main.js
@@ -1,15 +1,43 @@
+//Exercício 1
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+async function umPorSegundo() {
+    await delay();
+    console.log('1s');
+    await delay();
+    console.log('2s');
+    await delay();
+    console.log('3s');
+}
+
+umPorSegundo();
+
+//Exercício 2
 import axios from 'axios';
 
-class Api {
-    static async getUserInfo(userName) {
+async function getUserFromGithub(userName) {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${userName}`);
+        console.log(response.data);
+    } catch (err) {
+        console.warn('Usuário não existe');
+    }
+}
+
+getUserFromGithub('iuryoliveira');
+getUserFromGithub('iuryoliveira1231321');
+
+//Exercício 3
+class Github {
+    static async getRepositories(repo) {
         try {
-            const response = await axios.get(`https://api.github.com/users/${userName}`);
-            
-            console.log(response);
+            const response = await axios.get(`https//api.github.com/repos/${repo}`);
+            console.log(await response.data);
         } catch (err) {
-            console.warn('Erro na API');
+            console.log('Repositório não existe');
         }
     }
 }
 
-Api.getUserInfo('iuryoliveira');
+Github.getRepositories('rocketseat/rocketseat.com.br');
+Github.getRepositories('rocketseat/dslkvmskv');
